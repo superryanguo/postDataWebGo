a3be7e6b67c0d2fed5cfa49c3665b01c08a832cd
superryanguo <superryanguo@gmail.com>
2019-07-11 21:09:04 +0800

    first proto type done

--- postDataWebGo.go
+++ postDataWebGo.go
@@ -1,90 +1,79 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/sessions"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type PostContext struct {
+type DataContext struct {
 	Context string
 }
 
-var Store = sessions.NewCookieStore([]byte("hpb"))
-
-func showBoard(pattern string, w http.ResponseWriter) error {
-	t, e := template.ParseFiles("./templates/board.html")
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusInternalServerError)
-		return e
-	}
-	e = t.Execute(w, PostContext{Context: "Pls input the data"})
-	if e != nil {
-		http.Error(w, e.Error(), http.StatusInternalServerError)
-		return e
-	}
-	return e
+func tokenCreate() string {
+	ct := time.Now().Unix()
+	h := md5.New()
+	io.WriteString(h, strconv.FormatInt(ct, 10))
+	token := fmt.Sprintf("%x", h.Sum(nil))
+	// fmt.Println("token created :", token)
+	return token
 }
-func rootHandler(w http.ResponseWriter, r *http.Request) {
+func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 	var e error
 	if r.Method == "GET" {
-		session, _ := Store.Get(r, "session")
-
-		if session.Values["act"] != "true" {
-			session.Values["act"] = "true"
-			e = session.Save(r, w)
-			if e != nil {
-				log.Fatal(e)
-			}
-			fmt.Println("write the session data")
-		}
-
-		e = showBoard("*", w)
+		t, e := template.ParseFiles("./templates/datapost.html")
 		if e != nil {
-			log.Print(e)
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+			return
+		}
+		token := tokenCreate()
+		expiration := time.Now().Add(365 * 24 * time.Hour)
+		cookie := http.Cookie{Name: "csrftoken", Value: token, Expires: expiration}
+		http.SetCookie(w, &cookie)
+		e = t.Execute(w, token)
+		if e != nil {
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+			return
 		}
 	} else if r.Method == "POST" {
-		session, e := Store.Get(r, "session")
+		e = r.ParseForm()
 		if e != nil {
-			log.Fatal(e)
+			http.Error(w, e.Error(), http.StatusInternalServerError)
+			return
 		}
-		if session.Values["act"] == "true" {
-			e = r.ParseForm()
-			if e != nil {
-				http.Error(w, e.Error(), http.StatusInternalServerError)
-				return
-			}
+		//
+		formToken := template.HTMLEscapeString(r.Form.Get("CSRFToken"))
+		cookie, e := r.Cookie("csrftoken")
+		if e != nil {
+			log.Print(e)
+			return
+		}
+		if formToken == cookie.Value {
 
-			//t := time.Now().Format("2006-01-02 15:04:05")
-			//n := strings.Split(r.RemoteAddr, ":")[0] + "-" + strings.TrimLeft(strings.Fields(r.UserAgent())[1], "(")
-			//uname := strings.TrimRight(n, ";")
-			// fmt.Println("uanme =", uname)
-			//p := PostData{UserName: uname, Content: r.Form["body"][0], Created: t}
-
-			e = showBoard("*", w)
-			if e != nil {
-				log.Print(e)
-			}
-			// session.Values["username"] = username
-
+			t := time.Now().Format("2006-01-02 15:04:05")
+			n := strings.Split(r.RemoteAddr, ":")[0] + "-" + strings.TrimLeft(strings.Fields(r.UserAgent())[1], "(")
+			uname := strings.TrimRight(n, ";")
+			fmt.Println("uanme =", uname, t)
 		} else {
-			fmt.Println("the session is not active go to root")
-			http.Redirect(w, r, "/", 302)
+			log.Print("form token mismatch")
 		}
+		http.Redirect(w, r, "/", 302)
 	} else {
-		http.Error(w, "Unknown HTTP Action", http.StatusInternalServerError)
-		return
-
+		log.Print("Unknown request")
+		http.Redirect(w, r, "/", 302)
 	}
+
 }
+
 func main() {
-	fmt.Println("vim-go")
-	//http.HandleFunc("/addpost/", AddPostHandler)
-	http.HandleFunc("/", rootHandler)
-	//http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates"))))
+	http.HandleFunc("/", PostDataHandler)
+	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates"))))
 	log.Print("Running the server on port 8091.")
 	log.Fatal(http.ListenAndServe(":8091", nil))
 }
