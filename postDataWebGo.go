41118eb13fe0a228980f263637f2830c4b6b844a
superryanguo <superryanguo@gmail.com>
2019-07-14 22:41:49 +0800

    get the data in and out

--- postDataWebGo.go
+++ postDataWebGo.go
@@ -14,7 +14,9 @@ import (
 )
 
 type DataContext struct {
-	Context string
+	Token  string
+	Binstr string
+	Decode string
 }
 
 func tokenCreate() string {
@@ -26,6 +28,9 @@ func tokenCreate() string {
 	return token
 }
 func PostDataHandler(w http.ResponseWriter, r *http.Request) {
+	var context DataContext
+	//context.Binstr = "01221212"
+	//context.Decode = "0sdfsdfdsfds1221212"
 	var e error
 	fmt.Println("the r.methond is", r.Method)
 	if r.Method == "GET" {
@@ -34,11 +39,11 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, e.Error(), http.StatusInternalServerError)
 			return
 		}
-		token := tokenCreate()
+		context.Token = tokenCreate()
 		expiration := time.Now().Add(365 * 24 * time.Hour)
-		cookie := http.Cookie{Name: "csrftoken", Value: token, Expires: expiration}
+		cookie := http.Cookie{Name: "csrftoken", Value: context.Token, Expires: expiration}
 		http.SetCookie(w, &cookie)
-		e = t.Execute(w, token)
+		e = t.Execute(w, context)
 		if e != nil {
 			http.Error(w, e.Error(), http.StatusInternalServerError)
 			return
@@ -52,6 +57,8 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 
 		r.ParseMultipartForm(32 << 20) //defined maximum size of file
 		formToken := template.HTMLEscapeString(r.Form.Get("CSRFToken"))
+		context.Binstr = template.HTMLEscapeString(r.Form.Get("bodyin"))
+		fmt.Println("data in:", context.Binstr)
 		cookie, e := r.Cookie("csrftoken")
 		if e != nil {
 			log.Print(e)
@@ -73,12 +80,11 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 				}
 				defer f.Close()
 				io.Copy(f, file)
-				fmt.Println("upload a file done!")
 
 				t := time.Now().Format("2006-01-02 15:04:05")
 				n := strings.Split(r.RemoteAddr, ":")[0] + "-" + strings.TrimLeft(strings.Fields(r.UserAgent())[1], "(")
 				uname := strings.TrimRight(n, ";")
-				fmt.Println("uanme =", uname, t)
+				fmt.Println(uname, "upload a file done!", t)
 
 			} else {
 				log.Fatal("Can't create the data source file")
@@ -97,7 +103,7 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", PostDataHandler)
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("./templates"))))
-	http.Handle("/files/", http.StripPrefix("/files/", http.FileServer(http.Dir("./files"))))
+	http.Handle("/srcproto/", http.StripPrefix("/srcproto/", http.FileServer(http.Dir("./srcproto"))))
 	log.Print("Running the server on port 8091.")
 	log.Fatal(http.ListenAndServe(":8091", nil))
 }
