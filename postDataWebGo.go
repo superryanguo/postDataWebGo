a9eed76c114da83727eb126995a7ad3053c7be26
superryanguo <superryanguo@gmail.com>
2019-07-20 18:50:05 +0800

    fix the same dir or file and use goto

--- postDataWebGo.go
+++ postDataWebGo.go
@@ -33,21 +33,23 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 	var e error
 	ti := time.Now().Format("2006-01-02 15:04:05")
 	if r.Method == "GET" {
-		var context DataContext
-		t, e := template.ParseFiles("./templates/datapost.html")
-		if e != nil {
-			http.Error(w, e.Error(), http.StatusInternalServerError)
-			return
-		}
-		context.Token = tokenCreate()
-		fmt.Println(ti, "the r.methond ", r.Method, "create token", context.Token)
-		expiration := time.Now().Add(365 * 24 * time.Hour)
-		cookie := http.Cookie{Name: "csrftoken", Value: context.Token, Expires: expiration}
-		http.SetCookie(w, &cookie)
-		e = t.Execute(w, context)
-		if e != nil {
-			http.Error(w, e.Error(), http.StatusInternalServerError)
-			return
+		if r.RequestURI != "/favicon.ico" {
+			var context DataContext
+			t, e := template.ParseFiles("./templates/datapost.html")
+			if e != nil {
+				http.Error(w, e.Error(), http.StatusInternalServerError)
+				return
+			}
+			context.Token = tokenCreate()
+			fmt.Println(ti, "the r.method ", r.Method, "create token", context.Token)
+			expiration := time.Now().Add(365 * 24 * time.Hour)
+			cookie := http.Cookie{Name: "csrftoken", Value: context.Token, Expires: expiration}
+			http.SetCookie(w, &cookie)
+			e = t.Execute(w, context)
+			if e != nil {
+				http.Error(w, e.Error(), http.StatusInternalServerError)
+				return
+			}
 		}
 	} else if r.Method == "POST" {
 		var context DataContext
@@ -62,13 +64,14 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 		formToken := template.HTMLEscapeString(r.Form.Get("CSRFToken"))
 		context.Binstr = template.HTMLEscapeString(r.Form.Get("bodyin"))
 		context.Token = formToken
+		n := strings.Split(r.RemoteAddr, ":")[0] + "-" + strings.TrimLeft(strings.Fields(r.UserAgent())[1], "(")
+		uname := strings.TrimRight(n, ";")
 		cookie, e := r.Cookie("csrftoken")
 		if e != nil {
 			log.Print(e)
-			return
+			context.Returncode = "cookie read error"
+			goto SHOW
 		}
-		n := strings.Split(r.RemoteAddr, ":")[0] + "-" + strings.TrimLeft(strings.Fields(r.UserAgent())[1], "(")
-		uname := strings.TrimRight(n, ";")
 		fmt.Printf("%s %s %s  with cookie token %s and form token %s\n",
 			ti, uname, r.Method, cookie.Value, context.Token)
 		fmt.Println("indata :\n", context.Binstr)
@@ -81,19 +84,25 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 			}
 			if handler != nil {
 				defer file.Close()
-
-				e = os.Mkdir("./runcmd/"+formToken, os.ModePerm)
-				if e != nil {
-					log.Println(e)
-					context.Returncode = "Can't create the dir!"
-					return
+				dir := "./runcmd/" + formToken
+				_, err := os.Stat(dir)
+				if err != nil {
+					if os.IsNotExist(err) {
+						e = os.Mkdir(dir, os.ModePerm)
+						if e != nil {
+							log.Println(e)
+							context.Returncode = "Can't create the dir!"
+							//return //TODO: should we return or go to the html show place?
+							goto SHOW
+						}
+					}
 				}
 				context.Returncode = "create the dir done"
 				f, e := os.OpenFile("./runcmd/"+formToken+"/my.proto", os.O_WRONLY|os.O_CREATE, 0666)
 				if e != nil {
 					log.Println(e)
 					context.Returncode = "Can't create the file!"
-					return
+					goto SHOW
 				}
 				defer f.Close()
 				io.Copy(f, file)
@@ -115,6 +124,7 @@ func PostDataHandler(w http.ResponseWriter, r *http.Request) {
 			log.Print("form token mismatch")
 			context.Returncode = "form token mismatch"
 		}
+	SHOW:
 		b, e := template.ParseFiles("./templates/datapost.html")
 		if e != nil {
 			http.Error(w, e.Error(), http.StatusInternalServerError)
